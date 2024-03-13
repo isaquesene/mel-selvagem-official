815fa0c13b371aeb207c74c0580f468c76ebce76
Kaua <kauanylander@gmail.com>
2024-03-12 22:58:11 -0300

    Task #63 - Texto Sobre nós + novo icone

--- src/components/About.js
+++ src/components/About.js
@@ -24,44 +24,67 @@ const About = () => {
             {title} <span className='text-primary-400'>.</span>
           </h2>
         </div>
+
         {/* subtitle 1 */}
         <p 
           className='md:text-body-md mb-12'
           data-aos='fade-up'
           data-aos-delay='200'
         >
-          <b>Nós, apicultores,</b> acreditamos que criar abelhas é a nossa nossa contribuição com o universo e um compromisso que ultrapassa a produção de mel. Se é verdade que somos o que comemos, produzir um alimento que gera <b>saúde e bem estar</b> faz toda a diferença.
+          <b>Bem-vindo ao Mundo Dourado da Apicultura!</b>
           {/*subtitle1*/}
         </p>
-        {/* subtitle 2.1 */}
+
+        {/* subtitle 2 */}
         <p 
           className='md:text-body-md mb-8'
           data-aos='fade-up'
           data-aos-delay='300'
         >
-          <b>Somos apicultores </b>
-           por admirarmos este animal magnífico, de mais de 100 milhões de anos, socialmente organizado e fundamental agente de polinização, ao buscar pólen e nectar no vai e vem incansável de suas visitas às flores.
+          Seja bem-vindo ao nosso fascinante universo, onde a natureza se une à tradição e à paixão pela apicultura. No nosso site, você encontrará uma seleção exclusiva dos mais puros e deliciosos méis, colhidos cuidadosamente em nossas colmeias, localizadas em paisagens idílicas.
           {/*subtitle2*/}
         </p>
-        {/* subtitle 2.2*/}
+
+        {/* subtitle 3*/}
         <p 
           className='md:text-body-md mb-8'
           data-aos='fade-up'
           data-aos-delay='300'
         >
-          <b>Somos responsáveis</b>
-          , proporcionamos às abelhas as melhores condições de conforto e proteção, para que façam o que melhor sabem, aproveitando a exuberante natureza da <b>Serra Gaúcha brasileira</b>, criadas livres e protegidas.
-          {/*subtitle21*/}
+          <b>Nossa missão</b> é trazer para a sua mesa não apenas um mel de alta qualidade, mas uma experiência sensorial única, que resgata o verdadeiro sabor da natureza. Cada gota de mel que produzimos é um tributo ao trabalho árduo das nossas abelhas e ao amor que dedicamos a cada colmeia.
+          {/*subtitle 3*/}
         </p>
-        {/* subtitle 3 */}
+
+        {/* subtitle 4 */}
         <p 
           className='md:text-body-md mb-8'
           data-aos='fade-up'
           data-aos-delay='300'
         >
-          <b>Abelhas são seres únicos;</b> O ser humano, que ao longo do tempo, domesticou os mais diversos animais, mas jamais conseguiu fazer o mesmo com este singelo inseto. São seres livres e permaneceram <b>selvagens e indomáveis</b>. Aprendemos a lidar e aplicar as boas técnicas da apicultura, mas elas nunca abdicaram de seu instinto selvagem. Desta percepção, nasce o nosso nome, “O mel Selvagem”
-          {/*subtitle3*/}
+          Explore nossa variedade de méis artesanais, cada um com suas notas distintas e aromas irresistíveis. Do suave e floral ao intenso e robusto, temos algo para todos os paladares. Além disso, nossa linha de produtos inclui não apenas méis puros, mas também cremes, geleias e produtos de cuidados com a pele, todos feitos com o mesmo padrão de excelência.
+          {/*subtitle 4*/}
         </p>
+
+        {/* subtitle 5 */}
+        <p 
+          className='md:text-body-md mb-8'
+          data-aos='fade-up'
+          data-aos-delay='300'
+        >
+          Comprar conosco não é apenas adquirir um produto, é se conectar a uma história de dedicação e respeito pela natureza. Nossas abelhas são nossas parceiras e cuidamos delas com o maior carinho, garantindo que cada colheita seja sustentável e benéfica para o meio ambiente.
+          {/*subtitle 5*/}
+        </p>
+
+        {/* subtitle 6 */}
+        <p 
+          className='md:text-body-md mb-8'
+          data-aos='fade-up'
+          data-aos-delay='300'
+        >
+          Junte-se a nós nesta jornada doce e gratificante. <b className='text-primary-400'>Experimente a verdadeira essência do mel</b> e descubra por que somos apaixonados por apicultura. Seja bem-vindo ao mundo dourado da apicultura - sua jornada sensorial começa aqui!
+          {/*subtitle 6*/}
+        </p>
+
         {/* link */}
         <div
           data-aos='fade-up'
