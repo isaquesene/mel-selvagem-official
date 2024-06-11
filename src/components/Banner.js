1fe31e5950e99181a7f372dc50e53227655d06d4
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2024-06-10 22:25:35 -0300

    fix ajustes 22

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -50,7 +50,7 @@ const Banner = () => {
             </a>
           </div>
           <div 
-            className={`bg-banner bg-cover bg-no-repeat bg-left-top bg-center ${windowWidth <= 769 ? 
+            className={`bg-banner bg-cover bg-no-repeat bg-left bg-center ${windowWidth <= 769 ? 
               'absolute top-0 left-0 w-screen h-screen' : 
               'w-full h-full flex-1'}`}
             data-aos='fade-left' 
